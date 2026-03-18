0d00f83361d30fcf0be3826a7dd6b67504a4a9b9
liuyu <liuyu0123@outlook.com>
2026-03-18 17:58:42 +0800

    modify eval_water_val, save test result in csv

--- run.sh
+++ run.sh
@@ -41,7 +41,8 @@ python eval_water_val.py `
     --model_path ./checkpoints_water/best_water_seg.pth `
     --test-images D:\Files\Data\IRWSB\test\images `
     --test-masks D:\Files\Data\IRWSB\test\masks_white_noSuffix `
-    --save_pred_path ./eval_results
+    --save_pred_path ./eval_results `
+    --output checkpoints_water/test_result.csv
 
 #模型预测(单张图片推理)
 python demo_water.py --model_path ./checkpoints_water/best_water_seg.pth --input_path ./img/H05_1_0000000000.jpg --output_dir ./demo_results
